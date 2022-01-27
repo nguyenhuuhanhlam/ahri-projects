de6bbdbc3d07fe67c351de713d73460a40e6026b
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2022-01-27 23:33:46 +0700

    ...

--- frontends/next-platform/pages/_app.js
+++ frontends/next-platform/pages/_app.js
@@ -1,3 +1,5 @@
+import "../node_modules/@syncfusion/ej2-base/styles/material.css"
+import "../node_modules/@syncfusion/ej2-navigations/styles/material.css"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
